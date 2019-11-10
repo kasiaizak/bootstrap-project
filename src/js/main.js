30816426927429160d09b57e63b45b5e772e487c
Kasia Izak <kontakt@kasiaizak.pl>
2019-11-10 16:15:30 +0100

    fix name

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,10 @@
 if (document.body.classList.contains('bootstrap-project')) {
   console.log('bootstrap-project');
   window.onscroll = function () {
-    pokolorujMenu()
+    colorizeMenu()
   };
 
-  /*function pokolorujMenu() {
-    if (window.pageYOffset == 0) {
-      $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.0)'); ;
-    } else {
-      $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.6)');
-      $('.navbar--js').css('transition-duration', '1s');
-    }
-  }*/
-
-
-  function pokolorujMenu() {
+  function colorizeMenu() {
     var target = $('button[data-scrollto]').attr('data-scrollto');
     if (window.pageYOffset < $(target).offset().top) {
       $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.0)');;
@@ -27,7 +17,7 @@ if (document.body.classList.contains('bootstrap-project')) {
   }
 
   $('button[data-scrollto]').on('click', function (e) {
-    /*    e.preventDefault();*/
+    // e.preventDefault();
     var target = $(this).attr('data-scrollto');
     var topPosition = $(target).offset().top;
 
