a74d3813299df4897588206dd9a32c06ba286951
Kasia Izak <kontakt@kasiaizak.pl>
2019-11-10 17:28:54 +0100

    del filtering script

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,26 @@
 "use strict";
 
-if (document.body.classList.contains('bootstrap-project')) {
-  console.log('bootstrap-project');
-  window.onscroll = function () {
-    colorizeMenu()
-  };
+window.onscroll = function () {
+  colorizeMenu()
+};
 
-  function colorizeMenu() {
-    var target = $('button[data-scrollto]').attr('data-scrollto');
-    if (window.pageYOffset < $(target).offset().top) {
-      $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.0)');;
-    } else {
-      $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.6)');
-      $('.navbar--js').css('transition-duration', '1s');
-    }
+function colorizeMenu() {
+  var target = $('button[data-scrollto]').attr('data-scrollto');
+  if (window.pageYOffset < $(target).offset().top) {
+    $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.0)');;
+  } else {
+    $('.navbar--js').css('background-color', 'rgba(14, 216, 214, 0.6)');
+    $('.navbar--js').css('transition-duration', '1s');
   }
-
-  $('button[data-scrollto]').on('click', function (e) {
-    // e.preventDefault();
-    var target = $(this).attr('data-scrollto');
-    var topPosition = $(target).offset().top;
-
-    $('html, body').animate(
-      { scrollTop: topPosition },
-      600
-    );
-  });
 }
+
+$('button[data-scrollto]').on('click', function (e) {
+  // e.preventDefault();
+  var target = $(this).attr('data-scrollto');
+  var topPosition = $(target).offset().top;
+
+  $('html, body').animate(
+    { scrollTop: topPosition },
+    600
+  );
+});
